24d5bf9bc93f8d26841539a2b5270392c9bc0f08
manojava <manojava.koushik93@gmail.com>
2015-04-03 15:00:25 +0530

    Modifying routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-
+  resources :pictures
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  resources :pictures 
+  #root welcome#index
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
