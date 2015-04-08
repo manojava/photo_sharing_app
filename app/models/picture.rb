7682a2a2c3671cbf7d459056a06005851e9982e6
manojava <manojava.koushik93@gmail.com>
2015-04-08 20:38:36 +0530

    validating and writing test cases

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,5 @@
 class Picture < ActiveRecord::Base
+validates :name, presence: true
+validates :description, presence: true
+validates :place, presence: true
 end
--- test/models/picture_test.rb
+++ test/models/picture_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class PictureTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+   test "the truth" do
+     assert true
+   end
+
+   test "should not save picture without name" do 
+     picture= Picture.new 
+     assert_not picture.save 
+   end  
+
+   test "should not save picture without description" do
+     picture= Picture.new
+     assert_not picture.save
+   end
+
+   test "should not save picture without place" do
+     picture= Picture.new
+     assert_not picture.save
+   end
+
 end
